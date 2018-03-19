448661fab09c812537496e446e35dc396fbd5db5
Alexander Zeier <alex@hearthsim.net>
2018-03-19 17:44:31 +0100

    Add HearthSim.Common

--- Hearthstone Deck Tracker/PreBuildEvent.bat
+++ Hearthstone Deck Tracker/PreBuildEvent.bat
@@ -30,32 +30,12 @@ if %SkipGitSync% equ 1 (
 	goto ResourceCopyLabel
 )
 
-if exist "%SolutionDir%HearthDb" (
-  echo Updating "%SolutionDir%HearthDb" to origin/master
-  git -C "%SolutionDir%HearthDb" fetch
-  git -C "%SolutionDir%HearthDb" reset --hard origin/master
+if exist "%SolutionDir%HearthSim.Common" (
+  echo Updating "%SolutionDir%HearthSim.Common" to origin/master
+  git -C "%SolutionDir%HearthSim.Common" fetch
+  git -C "%SolutionDir%HearthSim.Common" reset --hard origin/master
 ) else (
-  git clone --depth 1 https://github.com/HearthSim/HearthDb.git "%SolutionDir%HearthDb"
-)
-
-echo.
-
-if exist "%SolutionDir%HearthMirror" (
-  echo Updating "%SolutionDir%HearthMirror" to origin/master
-  git -C "%SolutionDir%HearthMirror" fetch
-  git -C "%SolutionDir%HearthMirror" reset --hard origin/master
-) else (
-  git clone --depth 1 https://github.com/HearthSim/HearthMirror.git "%SolutionDir%HearthMirror"
-)
-
-echo.
-
-if exist "%SolutionDir%HSReplay-Api" (
-  echo Updating "%SolutionDir%HSReplay"-Api to origin/master
-  git -C "%SolutionDir%HSReplay-Api" fetch
-  git -C "%SolutionDir%HSReplay-Api" reset --hard origin/master
-) else (
-  git clone --depth 1 https://github.com/HearthSim/HSReplay-API-Client.git "%SolutionDir%HSReplay-Api"
+  git clone --depth 1 https://github.com/HearthSim/HearthSim.Common.git "%SolutionDir%HearthSim.Common"
 )
 
 echo.
--- bootstrap.bat
+++ bootstrap.bat
@@ -1,9 +1,10 @@
 @echo off
 
 nuget restore
-git clone https://github.com/HearthSim/HearthDb HearthDb
-git clone https://github.com/HearthSim/HearthMirror HearthMirror
-git clone https://github.com/HearthSim/HSReplay-API-Client.git HSReplay-Api
+git clone https://github.com/HearthSim/HearthSim.Common HearthSim.Common
+cd HearthSim.Common
+call bootstrap.bat
+cd ..
 git clone https://github.com/HearthSim/HDT-Localization -b collection HDT-Localization
 xcopy /Y "HDT-Localization\*.resx" "Hearthstone Deck Tracker\Properties\"
 ./generate_resources.bat
